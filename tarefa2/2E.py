556daadd7c13789939adc4bb1c801a5d95f95e3a
Julia Guilhermino <juh.guilhermino03@gmail.com>
2026-09-07 23:23:13 -0300

    feat: resposta da 2E com bug

---
+++ tarefa2/2E.py
@@ -0,0 +1,32 @@
+# TAREFA 2E: Exibir a imagem livro.jpg e somente a área do livro em uma nova imagem
+
+
+import cv2
+import numpy as np
+
+img = cv2.imread('imgs/livro.jpg')
+
+pts_origem = np.float32([
+    [142, 80],  
+    [520, 140], 
+    [210, 810],  
+    [640, 690]   
+])
+
+largura = 380
+altura = 480
+
+pts_destino = np.float32([
+    [0, 0],
+    [largura, 0],
+    [0, altura],
+    [largura, altura]
+])
+
+matriz = cv2.getPerspectiveTransform(pts_origem, pts_destino)
+livro_recortado = cv2.warpPerspective(img, matriz, (largura, altura))
+
+cv2.imshow('Imagem Original (livro.jpg)', img)
+cv2.imshow('Gabarito 2E - Livro Retificado', livro_recortado)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
